e414ac3aa385c69edaf7e3b7d3db26d00d57bc04
zhulong1999 <zhulong1999@email.com>
2020-02-24 19:25:51 +0800

    ‘使用Swoole开发飞机小游戏

---
+++ 飞机大战/index.php
@@ -0,0 +1,16 @@
+<?php
+    //建立游戏服务器
+    $ws = new swoole_websocket_server('0.0.0.0',9527);
+    $ws->on('open',function($ws,$client){
+        echo "开始游戏\n";
+    });
+    $ws->on('message',function($ws,$client){
+        echo $client->data."\n";
+    });
+    $ws->on('close',function(){
+        echo "游戏结束\n";
+    });
+    $ws->start();
+
+
+?>
